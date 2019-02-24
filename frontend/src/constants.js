df933a16829ee27bf9117114e6db63b41b1cd620
Beni <beni@beniurech.ch>
2019-02-24 21:25:47 +0100

    Added raw download method

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-// export const API_ROOT_URL = 'http://localhost:8000/api/';
-export const API_ROOT_URL = '/api/';
+export const API_ROOT_URL = 'http://localhost:8000/api/';
+// export const API_ROOT_URL = '/api/';
 export const CONTACT_EMAIL = 'malquarium@drbeni.ch';
 export const SITE_NAME = 'malquarium.org';
