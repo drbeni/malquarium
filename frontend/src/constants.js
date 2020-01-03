f970336349b58a76a8ba7208f6b68e58535ecafd
Beni Urech <github@drbeni.ch>
2020-01-03 16:07:11 +0100

    Fixed Python dependency handling in backend

--- frontend/src/constants.js
+++
@@ -1,4 +0,0 @@
-// export const API_ROOT_URL = 'http://localhost:8000/api/';
-export const API_ROOT_URL = '/api/';
-export const CONTACT_EMAIL = 'malquarium@drbeni.ch';
-export const SITE_NAME = 'malquarium.org';
