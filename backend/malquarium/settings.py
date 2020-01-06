4ee4a350b19611255bceb6f0b541660980fc000f
Beni Urech <github@drbeni.ch>
2020-01-06 20:35:33 +0100

    Fallback if mailserver port is not configured

--- backend/malquarium/settings.py
+++ backend/malquarium/settings.py
@@ -167,7 +167,7 @@ SWAGGER_SETTINGS = {
 
 # Email related settings
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 0))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
